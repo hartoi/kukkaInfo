22ddbc0ff7b82c5e16a25ba2418809e10ab0d4db
Mika Vainio <koti.mika.vainio@gmail.com>
2018-05-17 15:26:28 +0300

    EKa

---
+++ index.php
@@ -0,0 +1,21 @@
+<html><head><title>Kukkatieto</title>
+<link rel="stylesheet" href="tyylit.css">
+</head>
+<body>
+<div class="header">
+<h1>Kukkasivut</h1>
+</div>
+<div class="kukka">
+    <a href="ukonhattu.php"><h2>Ukonhattu</h2></a>
+    <p>Ukonhattu eli aitoukonhattu (Aconitum napellus) on monivuotinen...</p>
+</div>
+<div class="kukka">
+<a href="tarhakehakukka.php">
+    <h2>Tarhakehäkukka</h2>
+    </a>
+    <p>Tarhakehäkukka
+Tarhakehäkukka (Calendula officinalis) on yksivuotinen asterikasvien (Asteraceae) heimoon kuuluva kasvi, jota käytetään koriste- ja hyötykasvina...</p>
+</div>
+<?php include('footer.php') ?>
+</body>
+</html>
---
+++ ukonhattu.php
@@ -0,0 +1,14 @@
+<html><head><title>Kukkatieto</title>
+<link rel="stylesheet" href="tyylit.css">
+</head>
+<body>
+<div class="header">
+<h1>Kukkasivut</h1>
+</div>
+<div class="kukka">
+    <h2>Ukonhattu</h2>
+    <p>Ukonhattu eli aitoukonhattu (Aconitum napellus) on monivuotinen leinikkikasvi.[2] Sen lähisukulainen lehtoukonhattu (Aconitum lycoctonum) kasvaa villinä, mutta hyvin harvinaisena Etelä-Suomen lehdoissa. Nimensä kasvi on saanut suurien kukkien ylimpien terälehtien hattumaisesta muodosta.</p>
+</div>
+<?php include('footer.php') ?>
+</body>
+</html>
